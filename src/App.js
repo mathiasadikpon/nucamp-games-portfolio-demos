d3445b8f14e8e0a6ea485733dc6573a6ba1c10bd
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-02-22 05:34:57 -0500

    Add games list and update title in App component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+const gamesList = [
+  {
+    id: 0,
+    name: "Scythe",
+    rating: null,
+    category: "Fun",
+  },
+
+  {
+    id: 1,
+    name: "Machi Koro",
+    rating: null,
+    category: "Deck Building",
+  },
+  {
+    id: 2,
+    name: "Galaxy Trucker",
+    rating: null,
+    category: "Fun",
+  },
+];
 function App() {
   return (
     <div>
-      <h1>My App</h1>
+      <h1>My Board Games</h1>
+      {gamesList.map((game) => {
+        return (
+          <div>
+            <h4>{game.name}</h4> <p>{game.rating}</p> <p>{game.category}</p>{" "}
+          </div>
+        );
+      })}
     </div>
   );
 }
