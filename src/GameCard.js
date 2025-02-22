2d51b5291fd912b058e66ac59db840d3dbd14b4b
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-02-22 05:59:45 -0500

    Add Bootstrap for styling and update GameCard component with Bootstrap classes

--- src/GameCard.js
+++ src/GameCard.js
@@ -1,6 +1,6 @@
 const GameCard = ({ game }) => {
   return (
-    <div>
+    <div className="bg-ligth border p-4 m-2">
       <h4>{game.name}</h4> <p>{game.rating}</p> <p>{game.category}</p>{" "}
     </div>
   );
