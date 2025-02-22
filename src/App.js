b05a197ad57f897a3b3c2566025b478f6f3c9b0b
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-02-22 07:15:32 -0500

    Add category filter buttons to App component for better game selection

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ const gamesList = [
   },
 ];
 function App() {
-  const [categoryFilter, setCategoryFilter] = useState("Fun");
+  const [categoryFilter, setCategoryFilter] = useState("All");
   let filteredGames;
   if (categoryFilter === "All") {
     filteredGames = gamesList;
@@ -35,6 +35,27 @@ function App() {
   return (
     <div>
       <h1>My Board Games</h1>
+      <div>
+        <button
+          className="btn btn-primary me-2"
+          onClick={() => setCategoryFilter("All")}
+        >
+          All
+        </button>
+        <button
+          className="btn btn-primary me-2"
+          onClick={() => setCategoryFilter("Fun")}
+        >
+          Fun
+        </button>
+        <button
+          className="btn btn-primary"
+          onClick={() => setCategoryFilter("Deck Building")}
+        >
+          Deck Building
+        </button>
+      </div>
+
       {filteredGames.map((game) => {
         return <GameCard game={game} />;
       })}
