b713444c764b24c7ffe788a288c2dd3bdc5a43df
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-02-22 20:09:26 -0500

    Add React-Bootstrap for improved navigation styling and update App component layout

--- src/App.js
+++ src/App.js
@@ -4,20 +4,36 @@ import { TEST_GAMES } from "./TEST_GAMES";
 import RandomPage from "./components/RandomPage";
 import { Link, Route, Routes } from "react-router-dom";
 import GamePage from "./components/GamePage";
+import { Container, Nav, Navbar } from "react-bootstrap";
 
 function App() {
   const [gamesList, setGamesList] = useState(TEST_GAMES);
 
   return (
     <div>
-      <h1>My Board Games</h1>
-      <Link to="/" className="btn-link me-3">Home</Link>
-      <Link to="/random"className="btn-link">Random</Link>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand as={Link} to="/">
+                      My Board Games
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/">
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/random">
+              Random
+            </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
 
       <Routes>
         <Route path="/" element={<HomePage gamesList={gamesList} />} />
         <Route path="/random" element={<RandomPage gamesList={gamesList} />} />
-        <Route path="/games/:gameId" element={<GamePage gamesList={gamesList}/>}/>
+        <Route
+          path="/games/:gameId"
+          element={<GamePage gamesList={gamesList} />}
+        />
       </Routes>
     </div>
   );
